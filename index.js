23745ed7d576a9760898e2e96da6d43abdd59ee6
paorendor@gmail.com rendor <paorendor@gmail.com>
2018-09-09 21:07:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,28 +28,9 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-theFunk = function() 
+var theFunk = function() 
 {
-  
+  console.log ("FUNKY!")
 }
 
 
-
- describe('funkyFunction()', function() {
-    it('returns a function', function() {
-      expect(typeof window.funkyFunction()).toEqual('function')
-    })
-  })
-
-  describe('theFunk', function() {
-    it('is "FUNKY!"', function() {
-      expect(window.theFunk).toEqual('FUNKY!')
-    })
-
-
-
-
-
-
-
-  
