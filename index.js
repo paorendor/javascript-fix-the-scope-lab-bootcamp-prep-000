94a4c0cd34bce86553b914f3e7ef3c16e3114b33
paorendor@gmail.com rendor <paorendor@gmail.com>
2018-09-09 20:57:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = function()
 {
-  return "FUNKY!"
+  return function ()
+  {
+    return "FUNKY!"
+  }
 }
 
